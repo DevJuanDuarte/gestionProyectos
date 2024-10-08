61e8cf5f7290f1abfbc3a8b1f049b22e3dfdfbca
Juan <you@example.com>
2024-10-08 17:39:15 -0500

    Agregando rutas y instalando material

--- src/app/proyectos/proyectos-routing.module.ts
+++ src/app/proyectos/proyectos-routing.module.ts
@@ -1,7 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { ProyectoPageComponent } from './pages/proyecto-page/proyecto-page.component';
 
-const routes: Routes = [];
+//localhost:4200/proyectos
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent,
+    children: [
+      {
+        path: 'new-project',
+        component: NewPageComponent
+      },
+      {
+        path: 'edit/:id',
+        component: NewPageComponent
+      },
+      {
+        path: 'list',
+        component: ListPageComponent
+      },
+      {
+        path: ':id',
+        component: ProyectoPageComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'list'
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
